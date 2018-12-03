9c20aca6776e10b84f338a8fe49f47e9683f7f56
kvschaik <42870428+kvschaik@users.noreply.github.com>
2018-12-03 12:07:52 +0100

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  3 09:12:12 2018
+
+@author: koen1
+"""
+
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df1 = pd.read_csv('stepsdata.csv')
+df1.head()
+
+
+df2 = pd.read_csv('surveydata.csv')
+df2.head()
